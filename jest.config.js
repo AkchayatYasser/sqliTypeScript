c5be19edb161a25705064bfa175cf65ac119079e
Yasser Akchayat <yakchayat@smb.sqli.com>
2024-03-12 10:35:09 +0100

    jest added

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    preset: 'ts-jest', // Use ts-jest preset for TypeScript support
+    testEnvironment: 'jsdom', // Simulate a browser environment for tests (optional)
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // Specify file extensions to be processed
+  };
+  
